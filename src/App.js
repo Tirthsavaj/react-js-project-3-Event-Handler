d0dc152fa60de1dac24a17214ccb2713ddf56dd3
Tirthsavaj <tirthsavaj@email.com>
2023-08-07 23:11:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+  import logo from './logo.svg';
+  import './App.css';
+  import './components/design.css'
+  // import './components/Images'
+  import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    const allRecords = [
+      {
+        images: '/images/idli.jpg',
+        name: 'idli',
+        price: '30',
+        category: 'break-fast',
+      },
+      {
+        images: 'images/samosa.jpeg',
+        name: 'samosa',
+        price: '30',
+        category: 'lunch',
+      },
+      {
+        images: '/images/panjabi.jpg',
+        name: 'panjabi',
+        price: '99',
+        category: 'Dinner',
+      },
+      {
+        images: '/images/khaman.jpg',
+        name: 'khaman',
+        price: '120',
+        category: 'break-fast',
+      },
+      {
+        images: '/images/patra.avif',
+        name: 'patra',
+        price: '100',
+        category: 'lunch',
+      },
+      {
+        images: '/images/pizza.avif',
+        name: 'pizza',
+        price: '99',
+        category: 'Dinner',
+      },
+    ];
 
-export default App;
+    const [selectedCategory, setSelectedCategory] = useState();
+
+    const handleCategoryClick = (category) => {
+      setSelectedCategory(category);
+    };
+
+    const filteredRecords = selectedCategory
+      ? allRecords.filter((record) => record.category === selectedCategory)
+      : allRecords;
+
+    return (
+      <div className="App">
+        <div className="buttons">
+          <button onClick={() => handleCategoryClick()}>All</button>
+          <button onClick={() => handleCategoryClick('lunch')}>lunch</button>
+          <button onClick={() => handleCategoryClick('break-fast')}>break-fast</button>
+          <button onClick={() => handleCategoryClick('Dinner')}>Dinner</button>
+        </div>
+        <table className="records-table">
+          <thead>
+            <tr>
+              <th>Images</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+            filteredRecords.map((item) => {
+              const { images, name, price, category } = item;
+              return (
+                <tr key={name}>
+                  <td><img src={images} width={100} height={90}/></td>
+                  <td>{name}</td>
+                  <td>{price}</td>
+                  <td>{category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  export default App;
